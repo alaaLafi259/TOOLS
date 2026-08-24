c0be3a9c2ec27a4f13b9637fee3628660afa82e1
alaaLafi259 <3laalafi@gmail.com>
2026-08-24 21:15:22 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -4,12 +4,12 @@
 // Project Settings → General → Your apps → SDK setup and configuration
 // ============================================
 const firebaseConfig = {
-  apiKey: "ضع_قيمتك_هنا",
-  authDomain: "ضع_قيمتك_هنا",
-  projectId: "ضع_قيمتك_هنا",
-  storageBucket: "ضع_قيمتك_هنا",
-  messagingSenderId: "ضع_قيمتك_هنا",
-  appId: "ضع_قيمتك_هنا"
+  apiKey: "AIzaSyAUbcrNCCB2Y6kLTVeRv5ZscMgbrfUct3M",
+  authDomain: "tools-72703.firebaseapp.com",
+  projectId: "tools-72703",
+  storageBucket: "tools-72703.firebasestorage.app",
+  messagingSenderId: "609973046760",
+  appId: "1:609973046760:web:1f6a1432bc1c74d935285d"
 };
 
 firebase.initializeApp(firebaseConfig);
